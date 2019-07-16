7781248f5e78e525e854ebf01b06b8d92d0ea352
garfied <976680568@qq.com>
2019-07-16 20:27:54 +0800

    前端部分解决axios post方法的bug

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,86 @@
 <template>
   <div class="hello">
     <div class="title">
-      <div>11</div>
-      <div>22</div>
+      <div>Amer</div>
+      <div>wonderLand</div>
     </div>
-    <div>{{ msg }}</div>
-    <v-alert
-        :value="true"
-        type="success"
-      >
-        This is a success alert.
-      </v-alert>
-      <div>this is a new shit</div>
+    <div class="message">{{ msg }}</div>
+    <v-form v-model="valid">
+      <v-container>
+      <v-layout justify-center="true">
+          <v-flex md4 >
+              <v-text-field
+                class="border5"
+                label="Solo"
+                v-model="userName"
+                placeholder="Placeholder"
+                append-icon="search"
+                @click:append="toggleMarker"
+                solo
+              ></v-text-field>
+            </v-flex>
+      </v-layout>
+      </v-container>
+    </v-form>
+    <div>Searching for more</div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      valid: {},
+      userName: ''
+    }
+  },
+  methods: {
+    async toggleMarker() {
+      // const result = await axios.post('http://localhost:7001/userNameAuth')
+      const params = new URLSearchParams()
+      params.append('event_id', '123');
+      const result = await axios({
+        method: 'post',
+        url: 'http://localhost:7001/userNameAuth',
+        data: params
+      });
+      window.console.log(result)
+    }
   }
-}
+};
 </script>
 
 <style lang="less" scoped>
- .hello {
-   height: 100%;
-   display: flex;
-   flex-direction: column;
-   justify-content: space-around;
-   .title {
-     display: flex;
-     justify-content: space-between;
-     div {
-       &:nth-child(1) {
-         margin-left: 30px;
-       }
-       &:nth-child(2) {
-         margin-right: 30px;
-       }
-     }
-   }
- }
+.hello {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  font-size: 48px;
+  justify-content: space-around;
+  align-content: center;
+  .title {
+    display: flex;
+    justify-content: space-between;
+    div {
+      &:nth-child(1) {
+        margin-left: 30px;
+      }
+      &:nth-child(2) {
+        margin-right: 30px;
+      }
+    }
+  }
+  .border5 {
+    border-radius: 20%
+  }
+  .message {
+    margin-bottom: 100px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueJsonp from 'vue-jsonp'
 import Vuetify from 'vuetify'
+import axios from 'axios'
 
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 Vue.use(VueJsonp)
+axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 new Vue({
   render: h => h(App),
